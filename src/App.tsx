38ce8ec4904ccfbd42c4d1f808fb16cd14284f29
Fathurrohman Elkusnandi <felkusnandi@deloitte.com>
2025-07-18 14:28:59 +0700

    feat: Implement global context provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
+import { GlobalContextProvider } from "./context/GlobalContext.jsx"
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <Router />
+      <GlobalContextProvider>
+        <Router />
+      </GlobalContextProvider>
     </MantineProvider>
   );
 }
---
+++ src/context/GlobalContext.jsx
@@ -0,0 +1,27 @@
+import { useContext, createContext, useState, useEffect } from "react";
+
+const GlobalContext = createContext();
+
+export const GlobalContextProvider = ({ children }) => {
+    const getToken = localStorage.getItem("token");
+
+    const [token, setToken] = useState(getToken);
+
+    useEffect(() => {
+        if (token) {
+            console.log("Token found");
+        } else {
+            console.log("No token"); s
+        }
+    }, [token]);
+
+    return (
+        <GlobalContext.Provider value={{ token, setToken }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(GlobalContext)
+}
